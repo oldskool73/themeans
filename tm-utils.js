aa32b39712ff3d5d706ffc5498b780bc5092c9e9
amay0048 <anthonyjmay@MacBook-Pro.local>
2015-01-05 15:51:01 +1100

    Updates

--- tm-utils.js
+++ tm-utils.js
@@ -110,6 +110,44 @@ angular.module('tm.geolocation',[])
   });
 
 
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name airplayPutioApp.localstorage
+ * @description
+ * # localstorage
+ * Service in the airplayPutioApp.
+ */
+angular.module('tm.localstorage',[])
+  .service('tmLocalStorage', function tmLocalStorage($window) {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+
+    return {
+      set: function(key, value) {
+        $window.localStorage[key] = value;
+      },
+      get: function(key, defaultValue) {
+        var ret;
+        try
+        {
+          ret = angular.fromJson($window.localStorage[key])
+        }
+        catch(e)
+        {
+          ret = $window.localStorage[key];
+        }
+        return ret || defaultValue;
+      },
+      setObject: function(key, value) {
+        $window.localStorage[key] = angular.toJson(value);
+      },
+      getObject: function(key, defaultValue) {
+        return angular.fromJson($window.localStorage[key]) || defaultValue || {};
+      }
+    }
+  });
+
 /**
 @fileOverview
 
@@ -121,5 +159,6 @@ angular.module('tm.geolocation',[])
 
 angular.module('tm.utils', [
   'tm.geolocation',
-  'tm.consolelog'
+  'tm.consolelog',
+  'tm.localstorage'
 ]);
