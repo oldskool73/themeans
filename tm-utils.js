fd9e4d88ec6ee731995728221f540ffd8da65c55
amay0048 <anthonyjmay@MacBook-Pro.local>
2015-01-05 17:41:13 +1100

    Updates

--- tm-utils.js
+++ tm-utils.js
@@ -1,6 +1,6 @@
 /**
  * utils-demo - 
- * @version v0.0.0 - 2015-01-05
+ * @version v0.0.1 - 2015-01-05
  * @link 
  * @license 
  */
@@ -119,6 +119,60 @@ angular.module('tm.geolocation',[])
  * # localstorage
  * Service in the airplayPutioApp.
  */
+angular.module('tm.ionic-parse',['ionic'])
+  .service('Parse', function Parse($q, $window, $ionicPlatform) {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+
+    var deferred = $q.defer(),
+        self = this;
+
+    // override this function using a provider in the application config
+    this.getKeys = function(){
+      return {
+        applicationId: '',
+        javaScriptKey: '',
+        clientKey: ''
+      }
+    };
+
+    var parse = $window.Parse;
+    // Delete from the $window scope to ensure that we use the deps injection
+    delete $window.Parse; 
+
+    parse.initialize(
+      self.getKeys().applicationId,
+      self.getKeys().javaScriptKey
+    );
+
+    $ionicPlatform.ready(function(){
+      if($window.parsePlugin)
+      {
+        var bridge = $window.parsePlugin;
+        // Delete from the $window scope to ensure that we use the deps injection
+        delete $window.parsePlugin;
+        bridge.initialize(self.getKeys().applicationId, self.getKeys().clientKey, function()
+        {
+          deferred.resolve(bridge);
+        },function()
+        {
+          deferred.reject(bridge);
+        });
+      }
+    });
+
+    parse.nativeBridge = deferred.promise;
+    
+    return parse;
+  });
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name airplayPutioApp.localstorage
+ * @description
+ * # localstorage
+ * Service in the airplayPutioApp.
+ */
 angular.module('tm.localstorage',[])
   .service('tmLocalStorage', function tmLocalStorage($window) {
     // AngularJS will instantiate a singleton by calling "new" on this function
@@ -160,5 +214,6 @@ angular.module('tm.localstorage',[])
 angular.module('tm.utils', [
   'tm.geolocation',
   'tm.consolelog',
-  'tm.localstorage'
+  'tm.localstorage',
+  'tm.ionic-parse'
 ]);
