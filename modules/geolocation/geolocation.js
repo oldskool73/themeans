e952e18ff1db0c0c0f105b211e485343c8458725
Xaun Lopez <xaunlopez@gmail.com>
2015-01-19 12:30:50 +1100

    Moved tnGeoLocation into a Provider for api key config

--- modules/geolocation/geolocation.js
+++ modules/geolocation/geolocation.js
@@ -8,82 +8,72 @@
  * Service in the pressChampagneApp.
  */
 angular.module('tm.geolocation',[])
-  .service('tmGeoLocation', function GeoLocation($q, $http) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .provider('tmGeoLocation', function() {
 
-    this.currentPosition = function(){
+    // private object
+    var options = {
+      key: ''
+    };
+
+    // Public API for configuration
+    this.configure = function (configOptions) {
+      // merges objects together
+      angular.extend(options, configOptions);
+    };
+
+    // Method for instantiating
+    this.$get = function GeoLocation($q, $http) {
+      // AngularJS will instantiate a singleton by calling "new" on this function
+      this.currentPosition = function () {
         var deferred = $q.defer();
-
-        if (navigator.geolocation)
-        {
-        	(function(deferred){
-    	        navigator
-    	          .geolocation
-    	            .getCurrentPosition(function(position){
-                        deferred
-                            .resolve({
-                                latitude: position.coords.latitude.toFixed(4),
-                                longitude: position.coords.longitude.toFixed(4)
-                            });
-    	            });
-        	})(deferred);
-        }
-        else
-        {
-            deferred.reject({
-                message:'This platform doesn\'t support geolocation'
+        if (navigator.geolocation) {
+          (function (deferred) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+              deferred.resolve({
+                latitude: position.coords.latitude.toFixed(4),
+                longitude: position.coords.longitude.toFixed(4)
+              });
             });
+          }(deferred));
+        } else {
+          deferred.reject({ message: 'This platform doesn\'t support geolocation' });
         }
-
         return deferred.promise;
-    };
+      };
 
-    this.getAddressFromLatLng = function (position) {
-      var deferred = $q.defer();
-
-      var url = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyB_d9eqLvlGAGf1K4a7zJ6cJgdp9JqOts8&latlng=' +
-                position.latitude + ',' +
-                position.longitude;
-
-      $http.get(url).then(function (response) {
-        var data = response.data;
-
-        if (data.statusText === 'OK')
-        {
-          var address_components = data.results[0];
-          deferred.resolve(address_components);
-        }
-        else
-        {
-          if (data.status === 'ZERO_RESULTS')
-          {
-            deferred.reject({
-              message: 'We couldn\'t find your position. Please enter your address:',
-              response: data
-            });
+      this.getAddressFromLatLng = function (position) {
+        var deferred = $q.defer();
+        var url = 'https://maps.googleapis.com/maps/api/geocode/json?key='+options.key+'&latlng=' + position.latitude + ',' + position.longitude;
+        $http.get(url).then(function (response) {
+          var data = response.data;
+          if (data.status === 'OK' || data.statusText === 'OK') {
+            var address_components = data.results[0];
+            deferred.resolve(address_components);
+          } else {
+            if (data.status === 'ZERO_RESULTS') {
+              deferred.reject({
+                message: 'We couldn\'t find your position. Please enter your address:',
+                response: data
+              });
+            } else if (data.status === 'OVER_QUERY_LIMIT') {
+              deferred.reject({
+                message: 'Something went wrong, Please try again later.',
+                response: data
+              });
+            } else {
+              deferred.reject({
+                message: 'Something went wrong, Please try again later.',
+                response: data
+              });
+            }
           }
-          else if (data.status === 'OVER_QUERY_LIMIT')
-          {
-            deferred.reject({
-              message: 'Something went wrong, Please try again later.',
-              response: data
-            });
-          }
-          else
-          {
-            deferred.reject({
-              message: 'Something went wrong, Please try again later.',
-              response: data
-            });
-          }
-        }
-      }, function (data, status) {
-        deferred.reject("Something went wrong, Please try again.");
-      });
+        }, function (data, status) {
+          deferred.reject('Something went wrong, Please try again.');
+        });
+        return deferred.promise;
+      };
+      return this;
+    }
 
-      return deferred.promise;
-    };
-
-    return this;
   });
 
