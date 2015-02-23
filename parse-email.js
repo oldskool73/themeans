f7516fc443fff5f03eb9fc12e98164a915f78fb8
amay0048 <anthonyjmay@MacBook-Pro.local>
2015-02-23 17:01:34 +1100

    Travis commit : build undefined

--- parse-email.js
+++ parse-email.js
@@ -1,12 +1,51 @@
-// package/lib is a dependency we require
-// var lib = require( "package/lib" );
+module.exports = function(request, response, options){
 
-// behavior for our module
-function foo(){
-    console.log( "hello world!" );
-}
+  var primaryDomain = options.primaryDomain,
+      configObject  = options.emailConfig,
+      dataObject    = options.emailData;
 
-// export (expose) foo to other modules as foobar
-exports.foobar = foo;
+  var deferred      = new Parse.Promise();
+
+  var Mandrill = require('mandrill');
+  Mandrill.initialize('X7_fibq-tCUbHXVe4cfNVw');
+
+  var fs       = require('fs');
+  var Mustache = require('cloud/node_modules/mustache/mustache');
+  var template = fs.readFileSync(configObject.templateFile, 'utf8');
+
+  var html = Mustache.render(template, dataObject);
+  html = html.replace(/\/images\//ig, configObject.imageFixUrl);
+
+  Mandrill.sendEmail({
+    message: {
+      html: html,
+      subject: configObject.emailSubject,
+      from_email: configObject.fromEmail,
+      from_name: configObject.fromName,
+      to: [
+        {
+          email: dataObject.receiverEmail,
+          name: dataObject.receiverName
+        }
+      ]
+    },
+    async: true
+  }, {
+    success: function(httpResponse) {
+      // console.log('SUCCESS');
+      // console.log(httpResponse);
+      deferred.resolve(httpResponse);
+      // response.success();
+    },
+    error: function(httpResponse) {
+      // console.error('ERROR');
+      // console.log(httpResponse);
+      deferred.resolve(httpResponse);
+      // response.success();
+    }
+  });
+  return deferred;
+};
+
 define("parse-email/parse-email", function(){});
 
