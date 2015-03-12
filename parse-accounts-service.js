74cda5e7d82e12024b38d6fa18f724b13fd24ac3
Xaun Lopez <xaunlopez@gmail.com>
2015-03-12 18:53:09 +1100

    Travis commit : build undefined

--- parse-accounts-service.js
+++ parse-accounts-service.js
@@ -20,7 +20,8 @@ angular.module('tm.parseAccounts', [
   'Settings',
   'Parse',
   'tmLocalStorage',
-  function ($q, $timeout, Settings, Parse, tmLocalStorage) {
+  'CACHEDKEYS',
+  function ($q, $timeout, Settings, Parse, tmLocalStorage, CACHEDKEYS) {
     var _self = this;
     // Checks that account & roles are functioning properly.
     this.userAuthentication = function (user) {
@@ -33,6 +34,7 @@ angular.module('tm.parseAccounts', [
             deferred.reject({ message: 'Please try again, or contact system admin' });
             return;
           }
+          deferred.resolve();
         }, function (err) {
           console.error('Parse Error: ', err);
           deferred.reject({ message: 'Please try again in a few moments.' });
@@ -45,16 +47,20 @@ angular.module('tm.parseAccounts', [
     };
     // Returns an array of role names strings that this Parse User belongs to.
     this.getUserRoles = function (user) {
-      var deferred = $q.defer(), queryRoles = new Parse.Query(Parse.Role);
+      var deferred = $q.defer(), queryRoles = new Parse.Query(Parse.Role), cacheKey = CACHEDKEYS['roles:user'] + user.id;
       queryRoles.equalTo('users', user);
       queryRoles.find({
         success: function (response) {
+          if (!response.length) {
+            console.log('Parse Error: ', 'Parse User is not associated with any roles.');
+            deferred.reject({ message: 'There is an error with your account, please contact support.' });
+          }
           var roles = [];
           for (var i = 0; i < response.length; i++) {
             roles.push(response[i].get('name'));
           }
-          tmLocalStorage.setObject('user:roles:' + user.id, roles);
-          roles = tmLocalStorage.getObject('user:roles:' + user.id, []);
+          tmLocalStorage.setObject(cacheKey, roles);
+          roles = tmLocalStorage.getObject(cacheKey, []);
           deferred.resolve(roles);
         },
         error: function (response, err) {
@@ -78,9 +84,9 @@ angular.module('tm.parseAccounts', [
       return deferred.promise;
     };
     function getSettingsById(settingsId, edit) {
-      var deferred = $q.defer(), settingsQuery = new Parse.Query(Settings), ngSettings, cacheKey = 'settings:display:' + settingsId;
+      var deferred = $q.defer(), settingsQuery = new Parse.Query(Settings), cacheKey = CACHEDKEYS['settings'] + settingsId, ngSettings;
       if (edit) {
-        cacheKey = 'settings:edit:' + settingsId;
+        cacheKey = CACHEDKEYS['settings:edit'] + settingsId;
       }
       $timeout(function () {
         var cache = tmLocalStorage.getObject(cacheKey);
@@ -113,11 +119,11 @@ angular.module('tm.parseAccounts', [
       var deferred = $q.defer(), settings = new Settings(ngSettings, {
           ngModel: true,
           resetOpsQueue: false
-        });
+        }), cacheKey = CACHEDKEYS['settings:edit'] + settings.id;
       settings.save(null, {
         success: function (settings) {
-          tmLocalStorage.setObject(settings.id, settings);
-          settings = tmLocalStorage.getObject(settings.id);
+          tmLocalStorage.setObject(cacheKey, settings);
+          settings = tmLocalStorage.getObject(cacheKey);
           deferred.resolve(settings);
         },
         error: function (response, err) {
