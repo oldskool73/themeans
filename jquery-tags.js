9e061290141f71f36d29b867515a3c6a30dbd94e
Xaun Lopez <xaunlopez@gmail.com>
2015-03-17 17:50:31 +1100

    Travis commit : build undefined

--- jquery-tags.js
+++ jquery-tags.js
@@ -5,47 +5,50 @@
  * @description
  * # tagsInput
  */
-angular.module('tm.jquery-tags', []).directive('tmTagsInput', function () {
-  return {
-    restrict: 'A',
-    scope: {
-      ngModel: '=',
-      minChars: '=',
-      maxChars: '=',
-      placeholderColor: '=',
-      containerWidth: '=',
-      containerHeight: '=',
-      defaultText: '='
-    },
-    link: function postLink(scope, element) {
-      //@params(attr)
-      var $tags = jQuery(element).tagsInput({
-          height: scope.containerHeight,
-          width: scope.containerWidth,
-          onChange: function ($el) {
-            if (typeof $el.val === 'function') {
-              scope.$evalAsync(function () {
-                scope.ngModel = $el.val().toLowerCase().split(',');
-              });
-            }
-          },
-          interactive: true,
-          defaultText: scope.defaultText,
-          delimiter: [','],
-          removeWithBackspace: true,
-          minChars: scope.minChars,
-          maxChars: scope.maxChars,
-          placeholderColor: scope.placeholderColor
+angular.module('tm.jquery-tags', []).directive('tmTagsInput', [
+  '$window',
+  function ($window) {
+    return {
+      restrict: 'A',
+      scope: {
+        ngModel: '=',
+        minChars: '=',
+        maxChars: '=',
+        placeholderColor: '=',
+        containerWidth: '=',
+        containerHeight: '=',
+        defaultText: '='
+      },
+      link: function postLink(scope, element) {
+        //@params(attr)
+        var $tags = $window.jQuery(element).tagsInput({
+            height: scope.containerHeight,
+            width: scope.containerWidth,
+            onChange: function ($el) {
+              if (typeof $el.val === 'function') {
+                scope.$evalAsync(function () {
+                  scope.ngModel = $el.val().toLowerCase().split(',');
+                });
+              }
+            },
+            interactive: true,
+            defaultText: scope.defaultText,
+            delimiter: [','],
+            removeWithBackspace: true,
+            minChars: scope.minChars,
+            maxChars: scope.maxChars,
+            placeholderColor: scope.placeholderColor
+          });
+        scope.$watch('ngModel', function (newVal, oldVal) {
+          if (typeof oldVal === 'undefined') {
+            oldVal = [];
+          }
+          if (newVal && newVal.sort().toString() !== oldVal.sort().toString()) {
+            $tags.importTags('');
+            $tags.importTags(scope.ngModel.toString());
+          }
         });
-      scope.$watch('ngModel', function (newVal, oldVal) {
-        if (typeof oldVal === 'undefined') {
-          oldVal = [];
-        }
-        if (newVal && newVal.sort().toString() !== oldVal.sort().toString()) {
-          $tags.importTags('');
-          $tags.importTags(scope.ngModel.toString());
-        }
-      });
-    }
-  };
-});
+      }
+    };
+  }
+]);
