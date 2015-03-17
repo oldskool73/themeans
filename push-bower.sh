772be45daee3d25db7998e012369a0d1e6abc67b
amay0048 <anthonyjmay@MacBook-Pro.local>
2015-03-17 14:55:21 +1100

    Adjust parse.js to handle images properly

--- push-bower.sh
+++ push-bower.sh
@@ -1,1 +1,6 @@
-for dir in ./out/clones/subbower/*; do (cd "$dir" && git push); done
+grunt dist;
+grunt build:subbower;
+grunt publish:subbower;
+for dir in ./out/clones/subbower/*;
+do (cd "$dir" && git push);
+done
