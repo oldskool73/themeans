a59ad0f68e988aa1de02adcda7beb5808d5eb72c
Xaun Lopez <xaunlopez@gmail.com>
2015-04-15 01:49:05 +1000

    Travis commit : build undefined

---
+++ consolelog.js
@@ -0,0 +1,24 @@
+'use strict';
+/**
+ * @ngdoc service
+ * @name doctaApp.CL
+ * @description
+ * # CL
+ * Service in the doctaApp.
+ */
+angular.module('tm.consolelog', []).service('CL', [
+  '$log',
+  function CL($log) {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+    this.log = function () {
+      var handle, message, i, args = [];
+      for (i = 0; i < arguments.length; i++) {
+        args.push(arguments[i]);
+      }
+      handle = args.shift();
+      message = args.shift();
+      $log.log(handle, message, args);
+      return true;
+    };
+  }
+]);
