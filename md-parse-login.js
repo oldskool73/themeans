113babf579155e388221b9407628c9d018c0118a
Xaun Lopez <xaunlopez@gmail.com>
2015-06-30 16:48:19 +1000

    Travis commit : build undefined

--- md-parse-login.js
+++ md-parse-login.js
@@ -17,7 +17,7 @@ angular.module('tm.md-parse-login', [
     };
     MdParseLogin.prototype.link = function (scope, element) {
       //@params(scope, element, attrs)
-      var mainTmpl = '<div ' + scope.defaults.mainContainerAttributes + ' ng-switch="formType">' + '<md-toolbar ' + 'ng-if="!hideHeader" ' + 'ng-switch-when="login" ' + 'class="{{defaults.mdToolbarClass}}">' + '<div class="md-toolbar-tools">' + '<span>' + '{{defaults.loginToolbarText}}' + '</span>' + '</div>' + '</md-toolbar> ' + '<md-content ' + scope.defaults.loginMdContentAttributes + ' class="{{defaults.mdContentClass}} login" ' + 'ng-switch-when="login"> ' + '<form ng-submit="loginFormOnSubmit($event)"> ' + '<div ' + scope.defaults.loginInputsAttributes + '>' + '<md-input-container layout-fill> ' + '<label>Username</label> ' + '<input ' + 'required ' + 'type="text" ' + 'ng-model="user.username"> ' + '</md-input-container> ' + '<md-input-container layout-fill> ' + '<label>Password</label> ' + '<input ' + 'required ' + 'type="password" ' + 'ng-model="user.password"> ' + '</md-input-container> ' + '</div> ' + '<div ' + 'layout="row" ' + 'layout-align="center center">' + '<md-button flex ' + 'type="submit" ' + 'class="{{defaults.loginButtonClass}}"> ' + '{{defaults.loginButtonText}} ' + '</md-button> ' + '</div> ' + '</form> ' + '<p ' + 'layout="row" ' + 'layout-align="center center">' + '<a ng-click="switchFormOnClick(\'reset\')"> ' + 'Forgot password ' + '</a> ' + '</p> ' + '<div ' + 'layout="row" ' + 'layout-align="center center">' + '<md-button flex ' + 'type="button" ' + 'ng-bind="defaults.createButtonText" ' + 'class="{{defaults.createButtonClass}}" ' + 'ng-click="switchFormOnClick(\'create\')"> ' + '</md-button> ' + '</div> ' + '</md-content> ' + '<md-toolbar ' + 'ng-if="!hideHeader" ' + 'ng-switch-when="create" ' + 'class="{{defaults.mdToolbarClass}}"> ' + '<div class="md-toolbar-tools">' + '<span> ' + '{{defaults.createToolbarText}} ' + '</span> ' + '</div>' + '</md-toolbar> ' + '<md-content ' + scope.defaults.createMdContentAttributes + ' class="{{defaults.mdContentClass}} create-account" ' + 'ng-switch-when="create"> ' + '<form name="createForm" ng-submit="createFormOnSubmit($event)"> ' + '<div ' + scope.defaults.createInputsAttributes + ' >' + '<span flex layout-fill ng-include="createFormIncludeUrl"></span>' + '<md-input-container layout-fill> ' + '<label>Username</label> ' + '<input ' + 'required ' + 'name="username" ' + 'type="text" ' + 'ng-model="user.username"> ' + '</md-input-container> ' + '<div layout-fill ng-messages="createForm.username.$error" style="color:maroon;padding:10px;">' + '<div ng-message="required">You did not enter a user name</div>' + '</div>' + '<md-input-container layout-fill> ' + '<label>Email</label> ' + '<input ' + 'required ' + 'name="email" ' + 'ng-model="user.email" ' + 'type="email"> ' + '</md-input-container> ' + '<div layout-fill ng-messages="createForm.email.$error" style="color:maroon;padding:10px">' + '<div ng-message="required">You did not enter your email address</div>' + '</div>' + '<md-input-container layout-fill> ' + '<label>Password</label> ' + '<input ' + 'required ' + 'name="password" ' + 'ng-model="user.password" ' + 'type="password"> ' + '</md-input-container> ' + '<div layout-fill ng-messages="createForm.password.$error" style="color:maroon;padding:10px">' + '<div ng-message="required">You did not enter a password</div>' + '</div>' + '</div> ' + '<div  layout="row" layout-align="space-around center"> ' + '<md-button flex ' + 'type="submit" ' + 'ng-disabled="!createForm.$valid" ' + 'class="{{defaults.submitButtonClass}}"> ' + '{{defaults.submitButtonText}} ' + '</md-button> ' + '<span flex="5"></span>' + '<md-button flex ' + 'type="button" ' + 'class="{{defaults.backButtonClass}}" ' + 'ng-click="switchFormOnClick(\'login\')"> ' + '{{defaults.backButtonText}} ' + '</md-button> ' + '</div> ' + '</form> ' + '</md-content> ' + '<md-toolbar ' + 'ng-if="!hideHeader" ' + 'ng-switch-when="reset" ' + 'class="{{defaults.mdToolbarClass}}"> ' + '<div class="md-toolbar-tools">' + '<span> ' + '{{defaults.resetToolbarText}}' + '</span> ' + '</div>' + '</md-toolbar> ' + '<md-content ' + scope.defaults.resetMdContentAttributes + ' class="{{defaults.mdContentClass}} reset-password" ' + 'ng-switch-when="reset"> ' + '<form ng-submit="resetFormOnSubmit($event)"> ' + '<div ' + scope.defaults.resetInputsAttributes + ' >' + '<md-input-container layout-fill> ' + '<label>Email</label> ' + '<input ' + 'required ' + 'type="email" ng-model="user.email"> ' + '</md-input-container> ' + '</div> ' + '<div layout="row" layout-align="space-around center"> ' + '<md-button flex ' + 'type="submit" ' + 'ng-bind="defaults.submitButtonText" ' + 'class="{{defaults.submitButtonClass}}"> ' + '</md-button> ' + '<span flex="5"></span>' + '<md-button flex ' + 'type="button" ' + 'class="{{defaults.backButtonClass}}" ' + 'ng-click="switchFormOnClick(\'login\')"> ' + '{{defaults.backButtonText}} ' + '</md-button> ' + '</div> ' + '</form> ' + '</md-content> ' + '</div>';
+      var mainTmpl = '<div ' + 'id="md-toast-parent" ' + scope.defaults.mainContainerAttributes + ' ng-switch="formType">' + '<md-toolbar ' + 'ng-if="!hideHeader" ' + 'ng-switch-when="login" ' + 'class="{{defaults.mdToolbarClass}}">' + '<div class="md-toolbar-tools">' + '<span>' + '{{defaults.loginToolbarText}}' + '</span>' + '</div>' + '</md-toolbar> ' + '<md-content ' + scope.defaults.loginMdContentAttributes + ' class="{{defaults.mdContentClass}} login" ' + 'ng-switch-when="login"> ' + '<form ng-submit="loginFormOnSubmit($event)"> ' + '<div ' + scope.defaults.loginInputsAttributes + '>' + '<md-input-container layout-fill> ' + '<label>Username</label> ' + '<input ' + 'required ' + 'type="text" ' + 'ng-model="user.username"> ' + '</md-input-container> ' + '<md-input-container layout-fill> ' + '<label>Password</label> ' + '<input ' + 'required ' + 'type="password" ' + 'ng-model="user.password"> ' + '</md-input-container> ' + '</div> ' + '<div ' + 'layout="row" ' + 'layout-align="center center">' + '<md-button flex ' + 'type="submit" ' + 'class="{{defaults.loginButtonClass}}"> ' + '{{defaults.loginButtonText}} ' + '</md-button> ' + '</div> ' + '</form> ' + '<p ' + 'layout="row" ' + 'layout-align="center center">' + '<a ng-click="switchFormOnClick(\'reset\')"> ' + 'Forgot password ' + '</a> ' + '</p> ' + '<div ' + 'layout="row" ' + 'layout-align="center center">' + '<md-button flex ' + 'type="button" ' + 'ng-bind="defaults.createButtonText" ' + 'class="{{defaults.createButtonClass}}" ' + 'ng-click="switchFormOnClick(\'create\')"> ' + '</md-button> ' + '</div> ' + '</md-content> ' + '<md-toolbar ' + 'ng-if="!hideHeader" ' + 'ng-switch-when="create" ' + 'class="{{defaults.mdToolbarClass}}"> ' + '<div class="md-toolbar-tools">' + '<span> ' + '{{defaults.createToolbarText}} ' + '</span> ' + '</div>' + '</md-toolbar> ' + '<md-content ' + scope.defaults.createMdContentAttributes + ' class="{{defaults.mdContentClass}} create-account" ' + 'ng-switch-when="create"> ' + '<form name="createForm" ng-submit="createFormOnSubmit($event)"> ' + '<div ' + scope.defaults.createInputsAttributes + ' >' + '<span flex layout-fill ng-include="createFormIncludeUrl"></span>' + '<md-input-container layout-fill> ' + '<label>Username</label> ' + '<input ' + 'required ' + 'name="username" ' + 'type="text" ' + 'ng-model="user.username"> ' + '</md-input-container> ' + '<div layout-fill ng-messages="createForm.username.$error" style="color:maroon;padding:10px;">' + '<div ng-message="required">You did not enter a user name</div>' + '</div>' + '<md-input-container layout-fill> ' + '<label>Email</label> ' + '<input ' + 'required ' + 'name="email" ' + 'ng-model="user.email" ' + 'type="email"> ' + '</md-input-container> ' + '<div layout-fill ng-messages="createForm.email.$error" style="color:maroon;padding:10px">' + '<div ng-message="required">You did not enter your email address</div>' + '</div>' + '<md-input-container layout-fill> ' + '<label>Password</label> ' + '<input ' + 'required ' + 'name="password" ' + 'ng-model="user.password" ' + 'type="password"> ' + '</md-input-container> ' + '<div layout-fill ng-messages="createForm.password.$error" style="color:maroon;padding:10px">' + '<div ng-message="required">You did not enter a password</div>' + '</div>' + '</div> ' + '<div  layout="row" layout-align="space-around center"> ' + '<md-button flex ' + 'type="submit" ' + 'ng-disabled="!createForm.$valid" ' + 'class="{{defaults.submitButtonClass}}"> ' + '{{defaults.submitButtonText}} ' + '</md-button> ' + '<span flex="5"></span>' + '<md-button flex ' + 'type="button" ' + 'class="{{defaults.backButtonClass}}" ' + 'ng-click="switchFormOnClick(\'login\')"> ' + '{{defaults.backButtonText}} ' + '</md-button> ' + '</div> ' + '</form> ' + '</md-content> ' + '<md-toolbar ' + 'ng-if="!hideHeader" ' + 'ng-switch-when="reset" ' + 'class="{{defaults.mdToolbarClass}}"> ' + '<div class="md-toolbar-tools">' + '<span> ' + '{{defaults.resetToolbarText}}' + '</span> ' + '</div>' + '</md-toolbar> ' + '<md-content ' + scope.defaults.resetMdContentAttributes + ' class="{{defaults.mdContentClass}} reset-password" ' + 'ng-switch-when="reset"> ' + '<form ng-submit="resetFormOnSubmit($event)"> ' + '<div ' + scope.defaults.resetInputsAttributes + ' >' + '<md-input-container layout-fill> ' + '<label>Email</label> ' + '<input ' + 'required ' + 'type="email" ng-model="user.email"> ' + '</md-input-container> ' + '</div> ' + '<div layout="row" layout-align="space-around center"> ' + '<md-button flex ' + 'type="submit" ' + 'ng-bind="defaults.submitButtonText" ' + 'class="{{defaults.submitButtonClass}}"> ' + '</md-button> ' + '<span flex="5"></span>' + '<md-button flex ' + 'type="button" ' + 'class="{{defaults.backButtonClass}}" ' + 'ng-click="switchFormOnClick(\'login\')"> ' + '{{defaults.backButtonText}} ' + '</md-button> ' + '</div> ' + '</form> ' + '</md-content> ' + '</div>';
       var el = angular.element(mainTmpl), compiled = $compile(el);
       element.append(el);
       compiled(scope);
@@ -27,9 +27,7 @@ angular.module('tm.md-parse-login', [
       '$location',
       'Parse',
       '$mdToast',
-      '$mdDialog',
-      '$animate',
-      function ($scope, $location, Parse, $mdToast, $animate) {
+      function ($scope, $location, Parse, $mdToast) {
         var defaults = $scope.defaults = {};
         defaults.mdToolbarClass = 'md-primary md-default-theme';
         defaults.mdContentClass = 'md-padding';
@@ -64,8 +62,23 @@ angular.module('tm.md-parse-login', [
             return $scope.toastPosition[pos];
           }).join(' ');
         };
-        $scope.showSimpleToast = function (toastContent) {
-          $mdToast.show($mdToast.simple().content(toastContent).position($scope.getToastPosition()).hideDelay(2500));
+        $scope.showToastAlert = function (toastContent) {
+          $scope.toastMessage = toastContent;
+          var basicToastTemplate = '<md-toast>' + '<span flex>{{toastMessage}}</span>' + '</md-toast>';
+          var toastConfig = {
+              scope: $scope,
+              preserveScope: true,
+              bindToController: true,
+              hideDelay: parseInt($scope.customToastHideDelay) || 6000,
+              position: $scope.getToastPosition(),
+              parent: $scope.toastParentElementId ? angular.element(document.getElementById($scope.toastParentElementId)) : angular.element(document.getElementById('md-toast-parent'))
+            };
+          if ($scope.customToastTemplateUrl) {
+            toastConfig.templateUrl = $scope.customToastTemplateUrl;
+          } else {
+            toastConfig.template = basicToastTemplate;
+          }
+          $mdToast.show(toastConfig);
         };
         // default for ng-switch.
         $scope.formType = 'login';
@@ -81,11 +94,17 @@ angular.module('tm.md-parse-login', [
               $scope.onLoginSuccess(user);
             },
             error: function (user, error) {
+              // connection_failed
               if (error.code === 100) {
-                $scope.showSimpleToast('Please check your internet connnection and try again.');
+                $scope.showToastAlert('Please check your internet connnection and try again.');
                 return;
               }
-              $scope.showSimpleToast(error.message);
+              // object_not_found
+              if (error.code === 101) {
+                $scope.showToastAlert('Incorrect username or password.');
+                return;
+              }
+              $scope.showToastAlert(error.message);
             }
           });
         };
@@ -94,16 +113,15 @@ angular.module('tm.md-parse-login', [
             success: function () {
               $scope.sending = false;
               // Password reset request was sent successfully
-              $scope.showSimpleToast('An email with a link to reset your password has been sent.');
+              $scope.showToastAlert('An email with a link to reset your password has been sent.');
             },
             error: function (error) {
               $scope.sending = false;
               if (error.code === 100) {
-                $scope.showSimpleToast('Please check your internet connnection and try again.');
+                $scope.showToastAlert('Please check your internet connnection and try again.');
                 return;
               }
-              // Show the error message somewhere
-              $scope.showSimpleToast(error.message);
+              $scope.showToastAlert(error.message);
             }
           });
         };
@@ -126,11 +144,10 @@ angular.module('tm.md-parse-login', [
             error: function (user, error) {
               $scope.sending = false;
               if (error.code === 100) {
-                $scope.showSimpleToast('Please check your internet connnection and try again.');
+                $scope.showToastAlert('Please check your internet connnection and try again.');
                 return;
               }
-              // Show the error message somewhere and let the user try again.
-              $scope.showSimpleToast(error.message);
+              $scope.showToastAlert(error.message);
             }
           });
         };
