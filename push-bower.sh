26292b230842c1ad249a2ab014edd28fb7f44a14
Adam Robertson <oldskool73@gmail.com>
2016-02-11 17:48:17 +1100

    Make push-bower a bit nicer to use
    Can now push a single sub project by appending its name, like `./push-bower.sh bower-parse`

--- push-bower.sh
+++ push-bower.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
 grunt dist;
 grunt build:subbower;
 grunt publish:subbower;
-for dir in ./out/clones/subbower/*;
-do (cd "$dir" && git push && git push --tags);
-done
+if [ -z $1 ]
+then
+	for dir in ./out/clones/subbower/*;
+	do (cd "$dir" && git push && git push --tags);
+	done
+else
+	cd ./out/clones/subbower/$1;
+	git push && git push --tags;
+fi
